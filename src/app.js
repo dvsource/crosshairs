c653df326857451305009d2e40457a11095242d0
dvsource <virajkaush@gmail.com>
2021-12-25 18:28:48 +0530

    morgan request logging

--- src/app.js
+++ src/app.js
@@ -1,18 +1,42 @@
 const express = require('express');
 const winston = require('winston');
-
-const app = express();
-
-app.get('/test', (_, res) => res.send('test!!'));
+const morgan = require('morgan');
 
 const config = {
   env: 'development', // production | development
   port: 3200,
 };
 
+// LOGGER - morgan
+
+morgan.token('message', (_, res) => res.locals.errorMessage || '');
+
+const getIpFormat = () => (config.env === 'production' ? ':remote-addr - ' : '');
+const errorResponseFormat = `${getIpFormat()}:method :url :status - :response-time ms - message: :message`;
+const successResponseFormat = `${getIpFormat()}:method :url :status - :response-time ms`;
+
+const errorHandler = morgan(errorResponseFormat, {
+  skip: (_, res) => res.statusCode < 400,
+  stream: { write: (message) => logger.error(message.trim()) },
+});
+
+const successHandler = morgan(successResponseFormat, {
+  skip: (_, res) => res.statusCode >= 400,
+  stream: { write: (message) => logger.info(message.trim()) },
+});
+
+const app = express();
+
+app.use(successHandler);
+app.use(errorHandler);
+
+app.get('/test', (_, res) => res.send('test!!'));
+
 app.listen(config.port, () => {
   logger.info(`Listening to port ${config.port}`);
 });
+
+// LOGGER - winston
 
 const enumerateErrorFormat = winston.format((info) => {
   if (info instanceof Error) {
